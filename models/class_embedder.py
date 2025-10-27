c95155c785955ce8566eda4ea177ffd0fd5d1f59
Juhi Munmun Gupta <juhee.guptaa@gmail.com>
2025-10-27 00:14:51 -0400

    Updated class embedder

--- models/class_embedder.py
+++ models/class_embedder.py
@@ -7,7 +7,7 @@ class ClassEmbedder(nn.Module):
         super().__init__()
         
         # TODO: implement the class embeddering layer for CFG using nn.Embedding
-        self.embedding = None 
+        self.embedding = nn.Embedding(n_classes, embed_dim) 
         self.cond_drop_rate = cond_drop_rate
         self.num_classes = n_classes
 
@@ -16,8 +16,14 @@ class ClassEmbedder(nn.Module):
         
         if self.cond_drop_rate > 0 and self.training:
             # TODO: implement class drop with unconditional class
-            x = None
+            # randomly decide which samples to drop conditioning for
+            drop_mask = torch.rand(b, device=x.device) < self.cond_drop_rate
+            
+            # replace dropped classes with the "unconditional" class index
+            x = x.clone()
+            x[drop_mask] = self.num_classes  # index for unconditional embedding
+            #x = None
         
         # TODO: get embedding: N, embed_dim
-        c = None 
+        c = self.embedding(x)
         return c
